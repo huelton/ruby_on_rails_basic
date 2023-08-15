79d3ecaf1b725a27426ce1c7783750ecf0e72b29
hueltonsantos <huelton.santos.ext@vr.com.br>
2023-08-15 17:55:28 -0300

    inserindo programa para exibir numeros impares

---
+++ programa_exibir_numeros_impares.rb
@@ -0,0 +1,9 @@
+# PROGRAMA EXIBIR NUMEROS IMPARES
+limite_minimo = ARGV.first.to_i
+limite_maximo = ARGV[1].to_i
+
+
+(limite_minimo..limite_maximo).each do |numero|    
+        puts numero if numero.odd?
+end
+
